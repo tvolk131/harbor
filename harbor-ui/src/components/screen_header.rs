11035d022d01308a7b52818b16fcdf983ebb941f
Tommy Volk <tvolk131@gmail.com>
2025-09-06 21:33:03 -0500

    refactor: unindent header code

--- harbor-ui/src/components/screen_header.rs
+++ harbor-ui/src/components/screen_header.rs
@@ -13,96 +13,96 @@ pub fn h_screen_header(
     show_balance: bool,
     disable_switcher: bool,
 ) -> Element<Message> {
-    if let Some(item) = harbor.active_federation() {
-        let MintItem { name, id, .. } = item;
+    let Some(item) = harbor.active_federation() else {
+        return row![].spacing(16).into();
+    };
 
-        // Choose the right icon based on the mint type
-        let mint_icon = match id {
-            MintIdentifier::Cashu(_) => map_icon(SvgIcon::Squirrel, 24., 24.),
-            MintIdentifier::Fedimint(_) => map_icon(SvgIcon::People, 24., 24.),
-        };
+    let MintItem { name, id, .. } = item;
 
-        let mint_names: Vec<String> = harbor
-            .mint_list
-            .iter()
-            .filter(|m| m.active)
-            .map(|f| f.name.clone())
-            .collect();
+    // Choose the right icon based on the mint type
+    let mint_icon = match id {
+        MintIdentifier::Cashu(_) => map_icon(SvgIcon::Squirrel, 24., 24.),
+        MintIdentifier::Fedimint(_) => map_icon(SvgIcon::People, 24., 24.),
+    };
 
-        let is_generating = harbor.receive_status == ReceiveStatus::Generating;
-        let show_picker = !is_generating && !disable_switcher;
+    let mint_names: Vec<String> = harbor
+        .mint_list
+        .iter()
+        .filter(|m| m.active)
+        .map(|f| f.name.clone())
+        .collect();
 
-        let federation_element: Element<Message> = if show_picker {
-            pick_list(mint_names, Some(name.clone()), move |selected_name| {
-                if let Some(mint) = harbor.mint_list.iter().find(|f| f.name == selected_name) {
-                    Message::ChangeMint(mint.id.clone())
-                } else {
-                    Message::Noop
-                }
-            })
-            .menu_style(menu_style)
-            .style(borderless_pick_list_style)
-            .padding(Padding::from(16))
-            .handle(pick_list::Handle::Arrow {
-                size: Some(iced::Pixels(24.)),
-            })
-            .into()
-        } else {
-            container(text(name.clone()).size(16).style(|_theme| text::Style {
-                color: Some(Color::WHITE),
-            }))
-            .padding(Padding::from(16))
-            .into()
-        };
+    let is_generating = harbor.receive_status == ReceiveStatus::Generating;
+    let show_picker = !is_generating && !disable_switcher;
 
-        let current_federation = row![mint_icon, federation_element]
-            .align_y(Alignment::Center)
-            .spacing(16)
-            .width(Length::Shrink)
-            .padding(Padding::new(0.).left(16));
-        let formatted_balance = format_amount(item.balance);
-
-        let balance = row![text(formatted_balance).size(24)]
-            .align_y(Alignment::Center)
-            .padding(16);
-
-        let row = row![current_federation].spacing(16);
-
-        let secured = if harbor.tor_enabled {
-            row![
-                rich_text([
-                    span("Tor ").size(16).color(gray()),
-                    span("enabled").size(16).color(green()),
-                ])
-                .on_link_click(never),
-                map_icon(SvgIcon::Shield, 16., 16.)
-            ]
-        } else {
-            row![
-                rich_text([
-                    span("Tor ").size(16).color(gray()),
-                    span("disabled").size(16).color(red()),
-                ])
-                .on_link_click(never),
-                map_icon(SvgIcon::ShieldAlert, 16., 16.)
-            ]
-        }
-        .align_y(Alignment::Center)
-        .spacing(8)
-        .padding(Padding::new(0.).right(16));
-
-        column![
-            row.push_maybe(show_balance.then_some(vr()))
-                .push_maybe(show_balance.then_some(balance))
-                .push(horizontal_space())
-                .push(vr())
-                .push(secured)
-                .align_y(Alignment::Center)
-                .height(Length::Shrink),
-            hr()
-        ]
+    let federation_element: Element<Message> = if show_picker {
+        pick_list(mint_names, Some(name.clone()), move |selected_name| {
+            if let Some(mint) = harbor.mint_list.iter().find(|f| f.name == selected_name) {
+                Message::ChangeMint(mint.id.clone())
+            } else {
+                Message::Noop
+            }
+        })
+        .menu_style(menu_style)
+        .style(borderless_pick_list_style)
+        .padding(Padding::from(16))
+        .handle(pick_list::Handle::Arrow {
+            size: Some(iced::Pixels(24.)),
+        })
         .into()
     } else {
-        row![].spacing(16).into()
+        container(text(name.clone()).size(16).style(|_theme| text::Style {
+            color: Some(Color::WHITE),
+        }))
+        .padding(Padding::from(16))
+        .into()
+    };
+
+    let current_federation = row![mint_icon, federation_element]
+        .align_y(Alignment::Center)
+        .spacing(16)
+        .width(Length::Shrink)
+        .padding(Padding::new(0.).left(16));
+    let formatted_balance = format_amount(item.balance);
+
+    let balance = row![text(formatted_balance).size(24)]
+        .align_y(Alignment::Center)
+        .padding(16);
+
+    let row = row![current_federation].spacing(16);
+
+    let secured = if harbor.tor_enabled {
+        row![
+            rich_text([
+                span("Tor ").size(16).color(gray()),
+                span("enabled").size(16).color(green()),
+            ])
+            .on_link_click(never),
+            map_icon(SvgIcon::Shield, 16., 16.)
+        ]
+    } else {
+        row![
+            rich_text([
+                span("Tor ").size(16).color(gray()),
+                span("disabled").size(16).color(red()),
+            ])
+            .on_link_click(never),
+            map_icon(SvgIcon::ShieldAlert, 16., 16.)
+        ]
     }
+    .align_y(Alignment::Center)
+    .spacing(8)
+    .padding(Padding::new(0.).right(16));
+
+    column![
+        row.push_maybe(show_balance.then_some(vr()))
+            .push_maybe(show_balance.then_some(balance))
+            .push(horizontal_space())
+            .push(vr())
+            .push(secured)
+            .align_y(Alignment::Center)
+            .height(Length::Shrink),
+        hr()
+    ]
+    .into()
 }
